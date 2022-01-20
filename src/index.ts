d36d9bc6c0e480b10277b34497af603fc8f5dff6
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-20 13:35:51 -0300

    Instalando o ts-node-dev, para que a aplicação reinicei e veja se tem mudançã no código e se mantenha de pé

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import express, { Request, Response, NextFunction }  from "express";
 const app = express();
 
 app.get("/status", (req: Request, res: Response , next: NextFunction) => {
+    res.status(200).send( {foo: 'Hello World, Iaê'} );
+});
 
-
+app.listen(3000, () => {
+    console.log('Aplicação rodando na porta 3000');
 });
