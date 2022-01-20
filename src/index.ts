417e915a50a77afabb70de801ac0bde375f314ac
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-20 14:13:30 -0300

    Fazendo as requisições do users e do user/uuid no metodo get

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response, NextFunction }  from "express";
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
+app.use(usersRoute);
+
 app.get("/status", (req: Request, res: Response , next: NextFunction) => {
-    res.status(200).send( {foo: 'Hello World, Iaê'} );
+    res.status(200).send( {foo: 'Hello World, Sucesso !'} );
 });
 
 app.listen(3000, () => {
---
+++ src/routes/users.route.ts
@@ -0,0 +1,18 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    const users = [
+        {userName: 'Ricardo'}
+    ];
+    res.status(200).send( users );
+});
+
+usersRoute.get('/users/:uuid',  (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+
+    res.status(200).send( {uuid} );
+});
+
+export default usersRoute;
