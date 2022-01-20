1973ddafcffcbcba3e52b569d062b2a014fab29b
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-20 14:53:46 -0300

    Adicionando o método PUT

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import usersRoute from "./routes/users.route";
 
 const app = express();
 
+//Configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
+
+// Configuração das rotas
 app.use(usersRoute);
 
 app.get("/status", (req: Request, res: Response , next: NextFunction) => {
     res.status(200).send( {foo: 'Hello World, Sucesso !'} );
 });
 
+// Configuração do servidor
 app.listen(3000, () => {
     console.log('Aplicação rodando na porta 3000');
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
@@ -6,13 +7,32 @@ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [
         {userName: 'Ricardo'}
     ];
-    res.status(200).send( users );
+    res.status(StatusCodes.OK).send( users );
 });
 
 usersRoute.get('/users/:uuid',  (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
 
-    res.status(200).send( {uuid} );
+    res.status(StatusCodes.OK).send( {uuid} );
 });
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+
+    console.log(req.body);
+
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+
+    const modifieUser = req.body;
+
+    modifieUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send( { modifieUser } );
+});
+
 
 export default usersRoute;
