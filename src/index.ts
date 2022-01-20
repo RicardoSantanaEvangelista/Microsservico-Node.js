bcb9597ede5420b8e27b106e43995d5470ce6676
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-20 13:15:52 -0300

    Configurando o Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World");
+import express, { Request, Response, NextFunction }  from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response , next: NextFunction) => {
+
+
+});
