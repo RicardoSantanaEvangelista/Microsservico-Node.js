a689bc4e70d6ef5e20c83dfb8155613303c588fb
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-21 13:32:40 -0300

    Configurando o BD e criando as tabelas

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = "";
+const connectionString = "postgres://oguatyqy:KsQm3yE-Amp9tHdWv3foGtptXZVV3wXs@kesavan.db.elephantsql.com/oguatyqy";
 
 const db = new Pool({ connectionString });
 
---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,10 @@
+import User from "../models/user.model";
+
+class UserRepository {
+    findAllUsers(): User[] {
+        return [];
+    }
+
+}
+
+export default new UserRepository();
