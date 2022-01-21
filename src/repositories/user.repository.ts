55b53dfe982ba4f9f21421c8095bfbabd9295bd9
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-21 14:15:48 -0300

    Desenvolvendo a consulta de usuários por ID/UUID

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -12,11 +12,25 @@ class UserRepository {
            FROM application_user 
         `;
 
-        const result =  await db.query<User>(query);
-
-        const rows =  result.rows;
+        const { rows } =  await db.query<User>(query);
 
         return rows || [];
+    }
+
+    async findById(uuid: string): Promise<User> {
+        const query = `
+            SELECT uuid, username
+            FROM application_user
+            WHERE uuid = $1
+        `;
+
+        const values = [uuid];
+
+        const { rows } =  await db.query<User>(query, values);
+
+        const [user] = rows;
+
+        return user;
     }
 
 }
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -5,14 +5,19 @@ import userRepository from "../repositories/user.repository";
 const usersRoute = Router();
 
 usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
+
     const users =  await userRepository.findAllUsers();
+
     res.status(StatusCodes.OK).send( users );
 });
 
-usersRoute.get('/users/:uuid',  (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid',  async (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+
     const uuid = req.params.uuid;
 
-    res.status(StatusCodes.OK).send( {uuid} );
+    const user = await userRepository.findById(uuid);
+
+    res.status(StatusCodes.OK).send( user );
 });
 
 usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
