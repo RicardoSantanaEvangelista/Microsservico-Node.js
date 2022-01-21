b33e68614ef926c31a5df5679ce173d4a634db77
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-21 13:53:16 -0300

    Realizando uma consulta de usuários com o BD já implementado

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
+/*Criando tabelas do banco de dados*/
+
 CREATE TABLE IF NOT EXISTS application_user(
     uuid uuid DEFAULT uuid_generate_v4(),
     username VARCHAR NOT NULL,
@@ -8,5 +10,9 @@ CREATE TABLE IF NOT EXISTS application_user(
     PRIMARY KEY (uuid)
 )
 
+/*Criando os usuarios para complementar os dados*/
+
 INSERT INTO application_user (username, password) VALUES ('Ricardo', crypt('admin', 'my_salt'));
+
+INSERT INTO application_user (username, password) VALUES ('João', crypt('joao', 'my_salt'));
 
